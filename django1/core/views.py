f2ec6e4d4b56225d2c82ee55ab71035d23171f6a
Jamário Nascimento <jamariobatista@gmail.com>
2023-10-14 13:07:42 -0300

    404 and 500 Errors

--- django1/core/views.py
+++ django1/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
 from .models import Product
 
 
@@ -18,8 +20,19 @@ def contact(request):
 
 
 def product(request, pk):
-    productItem = Product.objects.get(id=pk)
+    # productItem = Product.objects.get(id=pk)
+    productItem = get_object_or_404(Product, id=pk)
     context = {
         'product': productItem
     }
     return render(request, 'product.html', context)
+
+
+def error404(request, ex):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html: charset=utf8', status=404)
+
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text/html: charset=utf8', status=500)
--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -116,3 +116,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # usando em produção
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGOUT_REDIRECT_URL = 'index'
